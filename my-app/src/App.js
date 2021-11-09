4e7373c618e2c11a0f4679943f7725a4290fea12
trung2612 <nguyenhatrung.nguyenha@gmail.com>
2021-11-09 14:52:06 +0700

    creat home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from "./components/layouts/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <Home/>
+    </>
   );
 }
 
