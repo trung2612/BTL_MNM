c936d5a0dd0ca1aca59487d553a25c62bba5ce8a
anhtuanqd <tuanqdqb@gmail.com>
2021-11-30 10:05:23 +0700

    updatemoi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,23 @@ import Product from "./components/Products/Products";
 import Contact from "./components/Contact/Contact";
 import About from "./components/AboutUs/About";
 import ContentLayout from "./components/layouts/ContentLayout/ContentLayout";
+import AoQuan from "./components/Products/productchild/Aoquan";
+import Vay from "./components/Products/productchild/Vay";
+import PhuKien from "./components/Products/productchild/Phukien";
+import Khac from "./components/Products/productchild/Khac";
+
 
 function App() {
   return (
     <ContentLayout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Product />} />
+        <Route path="/products" element={<Product />}>
+            <Route path="/products" element={<AoQuan />} />
+            <Route path="/products/vay" element={<Vay />} />
+            <Route path="/products/phukien" element={<PhuKien />} />
+            <Route path="/products/khac" element={<Khac />} />
+        </Route>
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
       </Routes>
--- my-app/src/components/Products/Products.js
+++ my-app/src/components/Products/Products.js
@@ -1,102 +1,56 @@
 import React from "react";
-import anh1 from '../../assets/images/product_01.jpg'
-import anh2 from '../../assets/images/product_02.jpg'
-import anh3 from '../../assets/images/product_03.jpg'
-import anh4 from '../../assets/images/product_04.jpg'
-import anh5 from '../../assets/images/product_05.jpg'
-import anh6 from '../../assets/images/product_06.jpg'
+
 import { Container } from "react-bootstrap";
+import {NavLink, Outlet } from 'react-router-dom';
 import Row from "../../components/atoms/Row/Row";
 import Col from "../../components/atoms/Col/Col";
-import ProductItem from "../../components/molecules/ProductItem/ProductItem";
-import commonStyle from "../../styles/commonStyle.module.css";
+
 import StyleProducts from "./Products.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
 
 
 const Products = () => {
   return (
     <>
-    <Container>
-      <Row>
-            <Col>
-            <div className={StyleProducts.filters}>
-              <ul className={StyleProducts.ul}>
-                  <li className={StyleProducts.active} >All Products</li>
-                  <li >Featured</li>
-                  <li >Flash Deals</li>
-                  <li >Last Minute</li>
-              </ul>
-            </div>  
-            </Col>       
-      </Row>
-      <Row>
-          <Col className={commonStyle.col4}>
-              <ProductItem
-                src={anh1}
-                title="Tittle goes here"
-                price="$30.25"
-                content="Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur."
-              />
-            </Col>
-            <Col className={commonStyle.col4}>
-              <ProductItem
-                src={anh2}
-                title="Tittle goes here"
-                price="$30.25"
-                content="Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur."
-              />
-            </Col>
-            <Col className={commonStyle.col4}>
-              <ProductItem
-                src={anh3}
-                title="Tittle goes here"
-                price="$30.25"
-                content="Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur."
-              />
-            </Col>
-            <Col className={commonStyle.col4}>
-              <ProductItem
-                src={anh4}
-                title="Tittle goes here"
-                price="$30.25"
-                content="Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur."
-              />
-            </Col>
-            <Col className={commonStyle.col4}>
-              <ProductItem
-                src={anh5}
-                title="Tittle goes here"
-                price="$30.25"
-                content="Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur."
-              />
-            </Col>
-            <Col className={commonStyle.col4}>
-              <ProductItem
-                src={anh6}
-                title="Tittle goes here"
-                price="$30.25"
-                content="Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur."
-              />
-            </Col>
-    </Row>
-    <Row>
-      <Col>
-          <div className={StyleProducts.navigate}>
-            <ul className={StyleProducts.pages}>
-              <li><div>1</div></li>
-              <li className={StyleProducts.active}><div>2</div></li>
-              <li><div>3</div></li>
-              <li><div>4</div></li>
-              <li><div><FontAwesomeIcon icon={faChevronCircleRight} color="#f33f3f" /></div></li>
-            </ul>
-          </div>
-      </Col>
-    </Row>
-    </Container>
-     </>
+      <Container>
+        <Row>
+              <Col>
+              <div className={StyleProducts.filters}>
+                <ul className={StyleProducts.ul}>
+                    <li ><NavLink className="" to="/products"  activeClassName={StyleProducts.active} exact >Áo Quần</NavLink></li>
+                    <li ><NavLink className="" to="/products/vay" activeClassName={StyleProducts.active} >Áo Len nè</NavLink></li>
+                    <li ><NavLink className="" to="/products/phukien" activeClassName={StyleProducts.active} >Phụ Kiện</NavLink></li>
+                    <li ><NavLink className="" to="/products/khac" activeClassName={StyleProducts.active} >Khác</NavLink></li>
+                </ul>
+              </div>  
+              </Col>       
+        </Row>
+        <Row>
+            <Outlet/>
+        </Row>
+        <Row>
+          <Col>
+              <div className={StyleProducts.navigate}>
+                <ul className={StyleProducts.pages}>
+                  <li><div>1</div></li>
+                  <li className={StyleProducts.active}><div>2</div></li>
+                  <li><div>3</div></li>
+                  <li><div>4</div></li>
+                  <li><div><FontAwesomeIcon icon={faAngleRight} color="#121212" /></div></li>
+                </ul>
+              </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
+
 export default Products;
+
